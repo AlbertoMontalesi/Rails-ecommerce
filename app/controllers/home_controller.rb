bc559ffd7c49e510f30ccd92d3296aec7f8a468b
AlbertoMontalesi <alberto.montalesi@outlook.it>
2017-05-28 17:57:53 +0700

    add display items from subcategory

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,4 @@
 class HomeController < ApplicationController
-
   def index
     @category = Category.all
   end
@@ -7,22 +6,35 @@ class HomeController < ApplicationController
   def clothes
     @category = Category.find_by_id(1)
     set_subcategory
+    current_subcategory
   end
 
   def shoes
     @category = Category.find_by_id(2)
     set_subcategory
+    current_subcategory
  end
 
   def accessories
     @category = Category.find_by_id(3)
     set_subcategory
+    current_subcategory
 end
 
-
- def set_subcategory
+  def set_subcategory
     @subcategory = Subcategory.where(category_id: @category.id)
+ end
+
+  def set_item
+    @items = Item.where(subcategory_id: @subcategory.ids)
   end
 
+def current_subcategory
+  if params[:sub].present?
+    @current_subcategory = Subcategory.find(params[:sub])
+    @items = @current_subcategory.items
+  end
+end
 
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
-  get  'shoes' => 'home#shoes'
+  get 'shoes' => 'home#shoes'
   get 'clothes' => 'home#clothes'
   get 'accessories' => 'home#accessories'
+
 
   resources :items
 end
