c97326d3c070aae49d251dd72f30c807d7167867
AlbertoMontalesi <alberto.montalesi@outlook.it>
2017-05-28 15:20:56 +0700

    modify navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
-  get  'Shoes' => 'home#shoes'
-  get 'Clothes' => 'home#clothes'
-  get 'Accessories' => 'home#accessories'
+  get  'shoes' => 'home#shoes'
+  get 'clothes' => 'home#clothes'
+  get 'accessories' => 'home#accessories'
 
   resources :items
 end
