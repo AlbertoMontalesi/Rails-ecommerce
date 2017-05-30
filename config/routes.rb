5207d27aabad3049a7e3097068980d6f9e9b1485
AlbertoMontalesi <alberto.montalesi@outlook.it>
2017-05-30 11:36:40 +0700

    customized routes for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :retailers
-  devise_for :users
+  devise_for :retailers, path:"", path_names: {sign_in: 'retailer_login', sign_out:'retailer_logout', sign_up:'retailer_register'}
+  devise_for :users, path:"", path_names: {sign_in: 'user_login', sign_out:'user_logout', sign_up:'user_register'}
   root 'home#index'
   get 'shoes' => 'home#shoes'
   get 'clothes' => 'home#clothes'
